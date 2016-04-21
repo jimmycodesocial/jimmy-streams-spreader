89a99cbcacabba164dfd1b9d031f588321500d4c
Alberto Menendez Romero <albertomr86@gmail.com>
2016-04-21 00:09:29 -0300

    Apache Storm bolt signature

--- src/main/java/com/jimmystreams/bolt/AudienceBolt.java
+++ src/main/java/com/jimmystreams/bolt/AudienceBolt.java
@@ -1,4 +1,19 @@
 package com.jimmystreams.bolt;
 
-public class AudienceBolt {
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Tuple;
+
+public class AudienceBolt extends BaseBasicBolt {
+
+    @Override
+    public void execute(Tuple input, BasicOutputCollector collector) {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+    }
 }
--- src/main/java/com/jimmystreams/bolt/SubscriptionsBolt.java
+++ src/main/java/com/jimmystreams/bolt/SubscriptionsBolt.java
@@ -1,4 +1,18 @@
 package com.jimmystreams.bolt;
 
-public class SubscriptionsBolt {
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Tuple;
+
+public class SubscriptionsBolt extends BaseBasicBolt {
+    @Override
+    public void execute(Tuple input, BasicOutputCollector collector) {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+    }
 }
