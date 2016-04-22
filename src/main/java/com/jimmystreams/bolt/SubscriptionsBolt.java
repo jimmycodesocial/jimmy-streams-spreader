965525f6cd5ae0bda79cc21403e0f52d677456b8
Alberto Menendez Romero <albertomr86@gmail.com>
2016-04-22 00:38:43 -0300

    Declaring fields in SubscriptionsBolt

--- src/main/java/com/jimmystreams/bolt/SubscriptionsBolt.java
+++ src/main/java/com/jimmystreams/bolt/SubscriptionsBolt.java
@@ -3,16 +3,24 @@ package com.jimmystreams.bolt;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 
-public class SubscriptionsBolt extends BaseBasicBolt {
-    @Override
-    public void execute(Tuple input, BasicOutputCollector collector) {
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
-    }
+public class SubscriptionsBolt extends BaseBasicBolt {
+    final static Logger logger = Logger.getLogger(SubscriptionsBolt.class);
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("stream", "activity"));
+    }
 
+    @Override
+    public void execute(Tuple input, BasicOutputCollector collector) {
+        // {"id": "<id>", "name": "<name>"}
+        JSONObject audience = (JSONObject)input.getValueByField("audience");
+        JSONObject activity = (JSONObject)input.getValueByField("activity");
     }
 }
