6dfdb253a2843621a262a270deff19b122e7fd67
Alberto Menendez Romero <albertomr86@gmail.com>
2016-04-23 03:03:42 -0300

    Adding doc to the bolts. Removing no necessary bolt.

--- src/main/java/com/jimmystreams/bolt/AudienceBolt.java
+++ src/main/java/com/jimmystreams/bolt/AudienceBolt.java
@@ -6,6 +6,8 @@
 
 package com.jimmystreams.bolt;
 
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
@@ -20,12 +22,31 @@ import org.json.JSONStringer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+/**
+ * Bolt that listen for activities and extract the implicit and explicit audiences.
+ * This bolt will emit a copy of the activity per each audience identified.
+ */
 public class AudienceBolt extends BaseBasicBolt {
-
+    /**
+     * These fields represent the implicit audience.
+     */
     private String[] implicitAudiences = {"actor", "object", "target"};
+
+    /**
+     * Fields with explicit audience.
+     */
     private String[] explicitAudiences = {"to", "bto", "cc", "bcc"};
 
+    /**
+     * Collector to ACK tuples.
+     */
+    private OutputCollector _collector;
+
+    /**
+     * Logger instance.
+     */
     final static Logger logger = Logger.getLogger(AudienceBolt.class);
 
     @Override
@@ -33,21 +54,28 @@ public class AudienceBolt extends BaseBasicBolt {
         declarer.declare(new Fields("audience", "activity"));
     }
 
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        super.prepare(stormConf, context);
+        this._collector = collector;
+    }
+
     @Override
     public void execute(Tuple input, BasicOutputCollector collector) {
-        // Activity JSON
+        // Activity JSON.
         JSONObject activity = (JSONObject)input.getValueByField("activity");
+        String activity_id = activity.getString("aid");
 
-        // Search the list of audiences present in the activity
+        // Search the list of audiences present in the activity.
         List<JSONObject> audiences = this.extractAudiencesFromActivity(activity);
+        logger.info(String.format("%d audiences detected in activity %s", audiences.size(), activity_id));
 
-        // Convert each audience specification to stream notation
+        // Convert each audience specification to stream notation.
         for (JSONObject audience: audiences) {
 
-            // Check for a correct definition of audience
+            // Check for a correct definition of audience.
             if (audience.has("id") && audience.has("objectType")) {
 
-                // Convert audience to stream notation
+                // Convert audience to stream notation.
                 String stream = new JSONStringer()
                         .object()
                             .key("name").value(audience.get("id"))
@@ -56,10 +84,13 @@ public class AudienceBolt extends BaseBasicBolt {
                         .toString();
 
                 // Emit the tuple <stream, activity>
-                logger.info(String.format("Audience: %s for activity: %s", stream, activity.get("aid")));
-                collector.emit(new Values(new JSONObject(stream), activity));
+                logger.info(String.format("Audience: %s for activity: %s", stream, activity_id));
+                this._collector.emit(new Values(new JSONObject(stream), activity));
             }
         }
+
+        // Ack the tuple.
+        this._collector.ack(input);
     }
 
     /**
--- src/main/java/com/jimmystreams/bolt/SubscriptionsBolt.java
+++ src/main/java/com/jimmystreams/bolt/SubscriptionsBolt.java
@@ -6,6 +6,8 @@
 
 package com.jimmystreams.bolt;
 
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
@@ -16,12 +18,31 @@ import org.apache.log4j.Logger;
 import org.apache.storm.tuple.Values;
 import org.json.JSONObject;
 
+import java.util.Map;
+
+/**
+ * Bolt that listen for audiences and retrieve the list of streams subscribed to the audience.
+ * This bolt will emit a copy of the activity per each stream subscribed.
+ */
 public class SubscriptionsBolt extends BaseBasicBolt {
+    /**
+     * Collector to ACK tuples.
+     */
+    private OutputCollector _collector;
+
+    /**
+     * Logger instance.
+     */
     final static Logger logger = Logger.getLogger(SubscriptionsBolt.class);
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
         declarer.declare(new Fields("stream", "activity"));
+    }
+
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        super.prepare(stormConf, context);
+        this._collector = collector;
     }
 
     @Override
@@ -31,6 +52,7 @@ public class SubscriptionsBolt extends BaseBasicBolt {
         JSONObject activity = (JSONObject)input.getValueByField("activity");
 
         // TODO: Don't emit the same audience as stream, instead of that, search all subscriptions.
-        collector.emit(new Values(audience, activity));
+        this._collector.emit(new Values(audience, activity));
+        this._collector.ack(input);
     }
 }
